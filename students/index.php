87670894e9c36b07b103be2f660430357d885abe
Sagar Samanta <sagarsamanta390@gmail.com>
2018-06-04 22:37:45 -0400

    files added

---
+++ students/index.php
@@ -0,0 +1,53 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <meta http-equiv="refresh" content="5"> -->
+    <link rel="stylesheet" href=".\css\index.css">
+    <title>Central Library</title>
+</head>
+<body>
+   <div class="main">
+      <div class="top-bar">
+          <div id="title">
+              <h1>
+                  Central Library
+              </h1>
+          </div>
+          <div class="top-bar-right">
+
+          </div>
+      </div>
+      <div class="slide-show">
+          <img src=".\images\slideshow.jpg" alt="Image">
+      </div>
+      <div class="container">
+          <div class="notification-left">
+            <p>notification 1</p>
+            <p>notification 1</p>
+            <p>notification 1</p>
+            <p>notification 1</p>
+          </div>
+          <div class="login-div">
+              <form action="#" class="login" id="login-form">
+                  <input type="text" name="email" id="email-input"><br>
+                  <input type="password" name="password" id="password"><br>
+                  <input type="button" value="login">
+              </form>
+          </div>
+          <div class="notifiaction-right">
+          <p>notification 1</p>
+            <p>notification 1</p>
+            <p>notification 1</p>
+            <p>notification 1</p>
+          </div>
+      </div>
+   </div> 
+</body>
+</html>
