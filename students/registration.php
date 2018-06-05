bb9ab3dd979447eab6e635a23b94b8c9eaf68791
Sagar Samanta <sagarsamanta390@gmail.com>
2018-06-05 10:10:35 -0400

    a

--- students/registration.php
+++ students/registration.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href=".\css\registration.css">
     <link rel="stylesheet" href="..\vendor\bootstrap-4.0.0-dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href=".\css\registration.css">
     <title>New Registration</title>
 </head>
 <body>
@@ -24,8 +24,8 @@
       <div class="content">
           <div class="registration-div">
               <form action="#" class="registration-form">
-                  <fieldset>
-                      <legend>New Registration</legend>
+                  <fieldset style="border: 1px solid #007BFF; padding: 20px;">
+                      <legend style="width: auto;">New Registration</legend>
                       <div class="form-group">
                           <label for="name">Name :</label>
                           <input type="text" name="name" id="name" class="form-control">
@@ -33,6 +33,14 @@
                       <div class="form-group">
                           <label for="email">Email :</label>
                           <input type="email" name="email" id="email" class="form-control">
+                      </div>
+                      <div class="form-group">
+                          <label for="password">New Password :</label>
+                          <input type="password" name="password" id="password" class="form-control">
+                      </div>
+                      <div class="form-group">
+                          <label for="confirm-password">Confirm Password :</label>
+                          <input type="password" name="confirm_password" id="confirm-password" class="form-control">
                       </div>
                   </fieldset>
               </form>
