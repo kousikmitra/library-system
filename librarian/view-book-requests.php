225a24866f8a10d4580bc391ed92225abb3dd182
arumoyadhikary <arumoy1997@gmail.com>
2018-07-28 14:41:17 +0530

    a

--- librarian/view-book-requests.php
+++ librarian/view-book-requests.php
@@ -26,7 +26,6 @@ if(isset($_GET['cancel']) and
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="./css/common.css">
-        <link rel="stylesheet" href="./css/search.css">
         <link rel="stylesheet" href="./css/view-book-requests.css">
         <?php include_once "./includes/bootstrap.php"; ?>
         <title>View Book Requests</title>
@@ -37,35 +36,14 @@ if(isset($_GET['cancel']) and
             <?php include "includes/topbar.php"; ?>
 
             <div class="content">
-            <div class="search-section">
-                        <form action="" method="get" class="form-inline">
-                        <h5 class="mr-sm-4">Search Students</h5>
-                            <select name="searchby" id="search-by" class="form-control mr-sm-4">
-                                <option value="name">Name</option>
-                                <option value="student_id">Student ID</option>
-                            </select>
-                            <input type="text" name="keyword" id="keyword" placeholder="Enter Search Keyword" value="<?php echo isset($_SESSION['search_key'])? $_SESSION['search_key'] : ""; ?>" class="form-control mr-sm-4" required>
-                            <input type="submit" value="Search" name="search" class="btn btn-primary mr-sm-4">
-                        </form>
-                    </div>
+                <div class="filter">
+
+                </div>
                 <div class="data">
                     <?php
                    
                    $sql = "SELECT id, s_id, requests.callno AS \"callno\", title, author, DATE_FORMAT(req_date,'%d %b %Y') AS \"req_date\", req_time, status FROM requests, books WHERE requests.callno=books.callno ORDER BY req_date, req_time";
-                   if(isset($_GET['search'])){
-                    $searchby = $_GET['searchby'];
-                    $keyword = $_GET['keyword'];
-                    $_SESSION['search_key'] = $_GET['keyword'];
-                    $_SESSION['searchby'] = $_GET['searchby'];
-                
-                    if($searchby === "name") {
-                        $sql = "SELECT id, requests.s_id AS \"s_id\", requests.callno AS \"callno\", title, author, DATE_FORMAT(req_date,'%d %b %Y') AS \"req_date\", req_time, status FROM requests, books, student WHERE requests.callno=books.callno AND requests.s_id=student.s_id AND student.s_name like '%$keyword%' ORDER BY req_date, req_time";
-                    } elseif($searchby === "student_id") {
-                        $sql = "SELECT id, s_id, requests.callno AS \"callno\", title, author, DATE_FORMAT(req_date,'%d %b %Y') AS \"req_date\", req_time, status FROM requests, books WHERE requests.callno=books.callno AND requests.s_id like '%$keyword%' ORDER BY req_date, req_time";
-                    } else {
-                        $sql = "SELECT id, s_id, requests.callno AS \"callno\", title, author, DATE_FORMAT(req_date,'%d %b %Y') AS \"req_date\", req_time, status FROM requests, books WHERE requests.callno=books.callno ORDER BY req_date, req_time";
-                    }
-                }
+               
                    $result = $conn->query($sql);
                    if($result->num_rows > 0){
                    ?>
@@ -129,10 +107,10 @@ if(isset($_GET['cancel']) and
                                 if($row['status'] == 0){
                                 ?>
                                                             <td>
-                                                                <a href="./issue-book.php?issue=<?php echo "{$row[ 'id']}"; ?>" style="color:green">
+                                                                <a href="./view-book-requests.php?issue=<?php echo " {$row[ 'id']}"; ?>" style="color:green">
                                                                     <i class="fa fa-check"></i>
                                                                 </a> &nbsp
-                                                                <a href="./view-book-requests.php?cancel=<?php echo " {$row[ 'id']}"; ?>" style="color:red">
+                                                                <a href="./view-book-requests.php?cancel=<?php echo " {$row[ 'id']}"; ?>" style="color:blue">
                                                                     <i class="fa fa-close"></i>
                                                                 </a>
                                                             </td>
